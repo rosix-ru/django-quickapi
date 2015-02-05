ce48680386169ed251cb57a47742f48fc84d3553
Grigoriy Kramarenko <root@rosix.ru>
2015-02-05 17:30:30 +1000

    append function get_vesion, append info for Django apps

--- quickapi/__init__.py
+++ quickapi/__init__.py
@@ -1,1 +1,8 @@
-__version__ = '2.5'
+VERSION = (2, 5, 1)
+
+def get_version(*args, **kwargs):
+    return '.'.join([ str(x) for x in VERSION ])
+
+__version__ = get_version()
+
+default_app_config = 'quickapi.apps.AppConfig'
