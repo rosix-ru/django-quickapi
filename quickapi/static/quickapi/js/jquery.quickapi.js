c3e3a3fbe15eb0c0e6822f19417d405ddc3352d6
Grigoriy Kramarenko <root@rosix.ru>
2015-02-13 15:29:03 +1000

    fixed jquery.quickapi.js

--- quickapi/static/quickapi/js/jquery.quickapi.js
+++ quickapi/static/quickapi/js/jquery.quickapi.js
@@ -32,12 +32,15 @@
      * })
      * 
      */
+
     $.quickAPI = function(options) {
+
         var options = options || new Object();
         
         options.args = options.args || { method: "quickapi.test" };
 
-        var settings = {
+        var jqxhr,
+            settings = {
                 type: options.type || "POST",
                 async: options.sync === true ? false : options.async === false ? false : true,
                 timeout: options.timeout || window.AJAX_TIMEOUT || 3000,
@@ -49,54 +52,83 @@
                 dataType: 'json',
             },
             callback = options.callback || function(json, status, xhr) {},
-            showAlert = options.handlerShowAlert || window.handlerShowAlert ||
-                function(head, msg, cls, cb) {
-                    if ($.type(msg) == 'object') {
-                        msg = $.toJSON(msg)
-                            .replace(/\,\"/g, ', "')
-                            .replace(/\"\:/g, '": ');
-                    }
-                    alert(head +'\n'+ msg);
-                    if (cb) { return cb() };
-                    return null;
-                },
-            jqxhr = $.ajax(settings)
+            showAlert = options.handlerShowAlert || window.handlerShowAlert || function(head, msg, cls, cb) {
+
+                var match;
+
+                if ($.type(msg) == 'object') {
+                    msg = $.toJSON(msg)
+                                .replace(/\,\"/g, ', "')
+                                .replace(/\"\:/g, '": ')
+                }
+                else if (msg.match(/<\!DOCTYPE/)) {
+                    match = msg.match(/<[title,TITLE]+>(.*)<\/[title,TITLE]+>/);
+                    if (match) head = match[1];
+
+                    match = msg.match(/<[body,BODY]+>([^+]*)<\/[body,BODY]+>/);
+                    if (match) msg = match[1]
+                                    .replace(/<\/?[^>]+>/g, '')
+                                    .replace(/ [ ]+/g, ' ')
+                                    .replace(/\n[\n]+/g, '\n')
+                }
+
+                if (msg.length > 512) {
+                    msg = msg.substring(0, 512) + ' ...'
+                };
+
+                alert(head +'\n'+ msg);
+
+                if (cb) { return cb() };
+
+                return null
+            };
+
+        jqxhr = $.ajax(settings)
                 // Обработка ошибок протокола HTTP
                 .fail(function(xhr, status, err) {
+
                     // Если есть переадресация, то выполняем её
                     if (xhr.getResponseHeader('Location')) {
-                        var location = xhr.getResponseHeader('Location')
-                            .replace(/\/[\#\-\w]*$/, "/?")
-                            .replace(/\?.*$/, "?next=" + window.location.pathname);
-                        window.location.replace(location);
-                        console.info("REDIRECT:" + xhr.getResponseHeader('Location'));
-                    } else {
-                        if (xhr.responseText) {
-                            // Иначе извещаем пользователя ответом и в консоль
-                            console.error("ERROR:" + xhr.responseText);
-                            var msg = (xhr.responseText.length <= 255) ?
-                                xhr.responseText : xhr.responseText.substring(0, 255) + '...';
-                            showAlert("ERROR:", msg, 'alert-danger');
-                        };
-                    };
+
+                        window.location.replace(
+                            xhr.getResponseHeader('Location')
+                                .replace(/\/[\#\-\w]*$/, "/?")
+                                .replace(/\?.*$/, "?next=" + window.location.pathname)
+                        );
+
+                        console.info("REDIRECT:" + xhr.getResponseHeader('Location'))
+
+                    } else if (xhr.responseText) {
+                        // Иначе извещаем пользователя ответом и в консоль
+                        console.error("ERROR:", xhr.responseText);
+
+                        showAlert("ERROR:", xhr.responseText, 'alert-danger')
+                    }
                 })
+
                 // Обработка полученных данных
                 .done(function(json, status, xhr) {
+
                     if (options.log && window.DEBUG) {console.debug(options.log)};
+
                     /* При переадресации нужно отобразить сообщение на некоторое время,
                      * а затем выполнить переход по ссылке, добавив GET-параметр для
                      * возврата на текущую страницу
                      */
                     if ((json.status >=300) && (json.status <400) && (json.data.Location != undefined)) {
+
                         var location = json.data.Location
-                            .replace(/\/[\#\-\w]*$/, "/?")
-                            .replace(/\?.*$/, "?next=" + window.location.pathname),
+                                .replace(/\/[\#\-\w]*$/, "/?")
+                                .replace(/\?.*$/, "?next=" + window.location.pathname),
                             redirect = function() { window.location.replace(location) };
+
                         console.info("REDIRECT:" + location);
+
                         if (json.message) {
-                            showAlert("REDIRECT:", json.message, 'alert-danger', redirect);
+                            showAlert("REDIRECT:", json.message, 'alert-danger', redirect)
+                        } else {
+                            redirect()
                         }
-                        else { redirect() };
                     }
                     /* При ошибках извещаем пользователя полученным сообщением */
                     else if (json.status >=400) {
@@ -108,17 +140,17 @@
                     else {
                         if (window.DEBUG) {
                             console.debug($.toJSON(json.message));
+
                             if (options.args.method == "quickapi.test") {
-                                o = new Object;
-                                $.extend(true, o, json.data);
-                                console.debug(o);
-                                showAlert('', o, 'alert-success');
-                            };
+                                console.debug(json.data)
+                            }
                         };
-                        return callback(json, status, xhr);
-                    };
+
+                        return callback(json, status, xhr)
+                    }
                 });
+
         return jqxhr
-    };
+    }
 
 }(jQuery));
