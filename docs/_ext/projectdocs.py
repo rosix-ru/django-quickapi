f691922299a4047ea136399a0c46bee95cbb8373
Grigoriy Kramarenko <root@rosix.ru>
2015-04-09 21:08:06 +1000

    append docs

---
+++ docs/_ext/projectdocs.py
@@ -0,0 +1,86 @@
+"""
+Sphinx plugins for Project documentation.
+"""
+import json
+import os
+import re
+
+from gettext import textdomain, bindtextdomain, gettext as _
+
+# set domain for gettext locallize this file
+domain = textdomain('ext')
+# set directory with localizations
+bindtextdomain(domain, localedir='locale')
+
+from sphinx import __version__ as sphinx_ver, addnodes
+from sphinx.writers.html import SmartyPantsHTMLTranslator
+
+
+class ProjectHTMLTranslator(SmartyPantsHTMLTranslator):
+    """
+    Project-specific reST to HTML tweaks.
+    """
+
+    # Don't use border=1, which docutils does by default.
+    def visit_table(self, node):
+        self.context.append(self.compact_p)
+        self.compact_p = True
+        self._table_row_index = 0  # Needed by Sphinx
+        self.body.append(self.starttag(node, 'table', CLASS='docutils'))
+
+    def depart_table(self, node):
+        self.compact_p = self.context.pop()
+        self.body.append('</table>\n')
+
+    def visit_desc_parameterlist(self, node):
+        self.body.append('(')  # by default sphinx puts <big> around the "("
+        self.first_param = 1
+        self.optional_param_level = 0
+        self.param_separator = node.child_text_separator
+        self.required_params_left = sum([isinstance(c, addnodes.desc_parameter)
+                                         for c in node.children])
+
+    def depart_desc_parameterlist(self, node):
+        self.body.append(')')
+
+    if sphinx_ver < '1.0.8':
+        #
+        # Don't apply smartypants to literal blocks
+        #
+        def visit_literal_block(self, node):
+            self.no_smarty += 1
+            SmartyPantsHTMLTranslator.visit_literal_block(self, node)
+
+        def depart_literal_block(self, node):
+            SmartyPantsHTMLTranslator.depart_literal_block(self, node)
+            self.no_smarty -= 1
+
+
+    version_text = {
+        'versionchanged': _('Changed in version %s'),
+        'versionadded': _('New in version %s'),
+    }
+
+    def visit_versionmodified(self, node):
+        self.body.append(
+            self.starttag(node, 'div', CLASS=node['type'])
+        )
+        version_text = self.version_text.get(node['type'])
+        if version_text:
+            title = "%s%s" % (
+                version_text % node['version'],
+                ":" if len(node) else "."
+            )
+            self.body.append('<span class="title">%s</span> ' % title)
+
+    def depart_versionmodified(self, node):
+        self.body.append("</div>\n")
+
+    # Give each section a unique ID -- nice for custom CSS hooks
+    def visit_section(self, node):
+        old_ids = node.get('ids', [])
+        node['ids'] = ['s-' + i for i in old_ids]
+        node['ids'].extend(old_ids)
+        SmartyPantsHTMLTranslator.visit_section(self, node)
+        node['ids'] = old_ids
+
